3b61eb20b9a265de86cb676441bb3327827aae8b
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-10-19 10:46:07 -0500

    changed register and login views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,33 @@
 @extends('layouts.master')
 
 @section('content')
-	<div class="row">
-		<div class="col-sm-6 col-sm-offset-3">
-			<form method="POST" action="{{ action('Auth\AuthController@postLogin') }}">
-				{{ csrf_field() }}
-				<div class="form-group">
-			    	<label for="email">Email:</label>
-			    	<input type="text" class="form-control" name="email" value="{{ old('email') }}">
-			  	</div>
-			  	@if($errors->has('email'))
-					<div class="alert alert-danger">
-						{{$errors->first('email')}}
-					</div>
-				@endif
-				<div class="form-group">
-			    	<label for="password">Password:</label>
-			    	<input type="password" class="form-control" name="password">
-			  	</div>
-			  	@if($errors->has('password'))
-					<div class="alert alert-danger">
-						{{$errors->first('password')}}
-					</div>
-				@endif
-			  	<button type="submit" class="btn btn-success">Login</button>
-			</form>
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<div class="jumbotron" style="width:90%">
+					<h3 class="form-signin-heading" >Login</h3>
+					<form method="POST" action="{{ action('Auth\AuthController@postLogin') }}" class="form-signin">
+						{{ csrf_field() }}
+						<div class="form-group">
+					    	<label for="email" >Email:</label>
+					    	<input type="text" placeholder="Email" class="form-control" name="email" value="{{ old('email') }}">
+					  	</div>
+					  	@if($errors->has('email'))
+							<div class="alert alert-danger">
+								{{$errors->first('email')}}
+							</div>
+						@endif
+						<div class="form-group">
+					    	<label for="password">Password:</label>
+					    	<input type="password" placeholder="Password" class="form-control" name="password">
+					  	</div>
+					  	@if($errors->has('password'))
+							<div class="alert alert-danger">
+								{{$errors->first('password')}}
+							</div>
+						@endif
+					  	<button type="submit" class="btn btn-success">Login</button>
+					</form>
+				</div>
 		</div>
 	</div>
 @stop
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,26 +10,29 @@
 	@endif
 	<div class="row">
 		<div class="col-sm-6 col-sm-offset-3">
-			<form method="POST" action="{{ action('Auth\AuthController@postRegister') }}">
-				{{ csrf_field() }}
-				<div class="form-group">
-			    	<label for="name">Name:</label>
-			    	<input type="text" class="form-control" name="name" value="{{ old('name') }}">
-			  	</div>
-				<div class="form-group">
-			    	<label for="email">Email:</label>
-			    	<input type="text" class="form-control" name="email" value="{{ old('email') }}">
-			  	</div>
-				<div class="form-group">
-			    	<label for="password">Password:</label>
-			    	<input type="password" class="form-control" name="password">
-			  	</div>
-				<div class="form-group">
-			    	<label for="password_confirmation">Password Confirmation:</label>
-			    	<input type="password" class="form-control" name="password_confirmation">
-			  	</div>
-			  	<button type="submit" class="btn btn-success">Register</button>
-			</form>
+			<div class="jumbotron" style="width:90%">
+				<h3 class="form-signin-heading" >Register</h3>
+				<form method="POST" action="{{ action('Auth\AuthController@postRegister') }}">
+					{{ csrf_field() }}
+					<div class="form-group">
+				    	<label for="name">Name:</label>
+				    	<input type="text" class="form-control" placeholder="Name" name="name" value="{{ old('name') }}">
+				  	</div>
+					<div class="form-group">
+				    	<label for="email">Email:</label>
+				    	<input type="text" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
+				  	</div>
+					<div class="form-group">
+				    	<label for="password">Password:</label>
+				    	<input type="password" class="form-control" placeholder="Password" name="password">
+				  	</div>
+					<div class="form-group">
+				    	<label for="password_confirmation">Password Confirmation:</label>
+				    	<input type="password" class="form-control" placeholder="Confirm Password" name="password_confirmation">
+				  	</div>
+				  	<button type="submit" class="btn btn-success">Register</button>
+				</form>
+			</div>
 		</div>
 	</div>
 @stop
